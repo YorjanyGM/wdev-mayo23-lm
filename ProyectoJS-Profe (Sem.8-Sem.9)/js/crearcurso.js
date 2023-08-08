9d26a3167b91baf8b6842d16d182e1d365c10763
YorjanyGM <134985656+YorjanyGM@users.noreply.github.com>
2023-08-07 21:37:19 -0600

    SoluciónParaEditarDatos

--- ProyectoJS-Profe (Sem.8-Sem.9)/js/crearcurso.js
+++ ProyectoJS-Profe (Sem.8-Sem.9)/js/crearcurso.js
@@ -3,14 +3,41 @@ const ModalSucces = new bootstrap.Modal(document.getElementById('ModalSuccess'))
 //la línea de arriba es la que se copia del comando modal (bs5-modal-default) en el HTML "crearcurso.html" (está al final en un script) -->
 //<--y se le pone el ID del primer div del modal, abajo de "if you want"
 
+var apibase = "https://paginas-web-cr.com/ApiPHP/apis/";
+var apicrear = "InsertarCursos.php";
+
 formulario.addEventListener('submit', function(e) //addEventListener() es para jalar la información del type del botón Enviar ("submit")-->
 //<--y se le agrega la "function(e)" para evitar que se recargue el alerta del modal
 {
     e.preventDefault(); //para evitar que la página se recargue automáticamente al ejercer un función de algún botón con MODAL
     //también es para evitar que se cierre el Modal.show
-    alert('creando');
-    ModalSucces.show(); //es la alerta del Modal diferente al "Alert" normal.
 
+    var datosEnviar = {
+        "nombre":document.getElementById('nombre').value ,
+        "descripcion":document.getElementById('descripcion').value ,
+        "tiempo":document.getElementById('tiempo').value ,
+        "usuario":"Yorjany Gamboa Mora",
+    }
 
+    apiurl = apibase + apicrear;
+        fetch(apiurl,
+            {
+                method:'POST',
+                body: JSON.stringify(datosEnviar)
+            })
+        .then(estructura => estructura.json())
+        .then((datosrespuesta) => {
+                ModalSucces.show()
+                completeInsert()
+                //completeDelete() (este comando solo se usa para eliminar) 
+            })
+        .catch(console.log);
+
+    //console.log(datosEnviar)
+    //alert('creando');
+    //ModalSucces.show(); //es la alerta del Modal diferente al "Alert" normal.
 });
 
+function completeInsert(){
+    window.location = 'listarcurso.html'
+}
--- ProyectoJS-Profe (Sem.8-Sem.9)/js/listarcurso.js
+++ ProyectoJS-Profe (Sem.8-Sem.9)/js/listarcurso.js
@@ -1,11 +1,44 @@
 var apibase = "https://paginas-web-cr.com/ApiPHP/apis/";
 var apiconsultar = "ListaCurso.php";
 var apieliminar = "BorrarCursos.php";
+var apieditar = "ActualizarCursos.php";
 
 const myModalEliminar = new bootstrap.Modal(document.getElementById('myModalEliminar'));
-
+const myModalEditar = new bootstrap.Modal(document.getElementById('myModalEditar'));
+const modalSuccess = new bootstrap.Modal(document.getElementById('modalSuccess'));
 
 let tablaresultado = document.querySelector('#tablaresultado');
+
+formulario.addEventListener('submit', function(e)
+{
+    e.preventDefault();
+    //alert('salvando')
+    var datosEnviar = {
+        "id":document.getElementById('id').value,
+        "nombre":document.getElementById('nombre').value ,
+        "descripcion":document.getElementById('descripcion').value ,
+        "tiempo":document.getElementById('tiempo').value ,
+        "usuario":"Yorjany Gamboa"
+    }
+
+    apiurl = apibase + apieditar;
+        fetch(apiurl,
+            {
+                method:'POST',
+                body: JSON.stringify(datosEnviar)
+            })
+        .then(estructura => estructura.json())
+        .then((datosrespuesta) => {
+            alert("Salvado")
+                //ModalSucces.show()
+                completeInsert() 
+            })
+        .catch(console.log);
+
+    //console.log(datosEnviar)
+    //alert('creando');
+    //ModalSucces.show(); //es la alerta del Modal diferente al "Alert" normal.
+});
 
 function consultardatos(){
     //fetch sirve para extraer, insertar modificar, eliminar consultardatos, 
@@ -32,6 +65,8 @@ function ajustardatostabla(datos){
                                 <td>${objetoindividual.tiempo}</td>
                                 <td>${objetoindividual.usuario}</td>
                                 <td>
+                                    <a name="Editar" id="Editar" class="btn btn-warning" role="button" onclick="mostrarEditarModal('${objetoindividual.id}','${objetoindividual.nombre}','${objetoindividual.descripcion}','${objetoindividual.tiempo}','${objetoindividual.usuario}')">Editar</a>
+                                    ||
                                     <a name="Eliminar" id="Eliminar" class="btn btn-danger" role="button" onclick="mostrarModal('${objetoindividual.id}')">Eliminar</a>
                                 </td>                              
             </tr>
@@ -74,4 +109,17 @@ function ajustardatostabla(datos){
         
     }
 
+    function mostrarEditarModal(id, nombre, descripcion, tiempo, usuario){
+        document.getElementById('id').value = id;
+        document.getElementById('nombre').value = nombre;
+        document.getElementById('descripcion').value = descripcion;
+        document.getElementById('tiempo').value = tiempo;
+        document.getElementById('usuario').value = usuario;
+        myModalEditar.show();
+    }
+
 consultardatos();
+
+//crear una funcion parecida a la del submit
+//cambiar el metodo de insertar por el de editar
+//crear una funcion similar a completeDelete
