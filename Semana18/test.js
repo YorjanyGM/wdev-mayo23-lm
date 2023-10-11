43f7631441b6677495701248b46c683e816f4fd3
YorjanyGM <134985656+YorjanyGM@users.noreply.github.com>
2023-10-11 07:27:21 -0600

    typescript

---
+++ Semana18/test.js
@@ -0,0 +1,30 @@
+const titulo = "Semana 18"
+console.log(titulo);
+console.log(typeof titulo);
+
+const numerocualquiera = 10;
+console.log(numerocualquiera);
+console.log(typeof numerocualquiera);
+
+const bolean = true;
+console.log(bolean);
+console.log(typeof bolean);
+
+//Estaticos, manejan tipo de dato
+//C++
+//java
+//C#
+
+//por ejemplo 
+//number variable = 0;
+
+//Dinámicos, no necesitan saber cual es el tipo de dato
+//js
+//python
+//var variable = 0;
+
+//comandos para terminal Code Runner
+//get-ExecutionPolicy consulta las políticas
+//get-ExecutionPolicy remotesigned asigna permisos de archivos remotos firmados
+
+//ts = typescript
